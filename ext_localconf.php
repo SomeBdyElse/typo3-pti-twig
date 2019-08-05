d81ba56715d2a417a96cb8852f454d9aa9e2c260
Hannes Lau <office@hanneslau.de>
2019-08-05 16:10:10 +0200

    [BUGFIX] Fix default view configuration

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,4 +5,4 @@ $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['pti_twig']['disableCache'] = false;
 $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['pti_twig']['rootTemplatePath'] = '';
 $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['pti_twig']['loader'] = [];
 
-$GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['pti']['view']['defaultView'] = \PrototypeIntegration\Twig\TwigView::class;
+$GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['pti']['defaultView'] = \PrototypeIntegration\Twig\TwigView::class;
