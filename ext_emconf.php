22e942f9b67cdc662570a21f6950e59919379148
Christian Wolfram <c.wolfram@chriwo.de>
2021-01-06 11:48:37 +0100

    [TASK] Drop support for TYPO3 9.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,4 +2,11 @@
 $EM_CONF['pti_twig'] = [
     'title' => 'PTI Twig',
     'description' => 'Prototype Integration - Twig',
+    'version' => '3.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '*',
+            'pti' => '*'
+        ],
+    ],
 ];
