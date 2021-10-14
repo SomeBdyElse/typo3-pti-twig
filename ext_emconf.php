2320d56559b17733c9fd29ddd1e2ff4a1eece768
Christian Wolfram <c.wolfram@chriwo.de>
2021-10-14 13:23:17 +0200

    [TASK] Migrate to TYPO3 10.4
    * Drop support for TYPO3 9.5
    * Remove static version from ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,4 +2,10 @@
 $EM_CONF['pti_twig'] = [
     'title' => 'PTI Twig',
     'description' => 'Prototype Integration - Twig',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '*',
+            'pti' => '*'
+        ],
+    ],
 ];
