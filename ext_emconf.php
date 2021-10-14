561950b0018e3f9933e14b814af75f41491f2d7d
Christian Wolfram <c.wolfram@chriwo.de>
2021-10-14 09:29:57 +0200

    [TASK] Remove version

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,6 @@
 $EM_CONF['pti_twig'] = [
     'title' => 'PTI Twig',
     'description' => 'Prototype Integration - Twig',
-    'version' => '3.0',
     'constraints' => [
         'depends' => [
             'typo3' => '*',
